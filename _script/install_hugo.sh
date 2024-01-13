51a623fa765090770359d25814bf12bf37ee71c6
Iain Dickson <icd@wan0.net>
2024-01-13 15:06:20 +1100

    Basis is now hopefully working!

---
+++ _script/install_hugo.sh
@@ -0,0 +1,2 @@
+wget https://github.com/gohugoio/hugo/releases/download/v0.121.2/hugo_0.121.2_linux-amd64.deb
+dpkg -i hugo_0.121.2_linux-amd64.deb
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wan0net/wan0.github.io
+
+go 1.18
+
+require github.com/jpanther/congo/v2 v2.7.6 // indirect
